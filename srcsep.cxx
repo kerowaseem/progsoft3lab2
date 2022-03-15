38bb60afa49f136a593bdc3c37e02c4e888c24ff
TorinStorkey <torinstorkey@tdeb>
2022-03-15 16:11:35 +0000

    finnished pre training and processing yet to write writer

--- srcsep.cxx
+++ srcsep.cxx
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <vector>
 #include <iostream>
-#include <list>
+#include <vector>
 #include <stdexcept>
+#include <math.h>
 //#include <sstream>
 #include "audioio.h"
 #include "Fir1.h"
@@ -20,12 +21,15 @@ int main(int argc, char* argv[]){
     float ss(4.9);
     float lrate(0.002);
     int nc(fl * sr);
+    int fnum (1);
+    int trial (2*sr); 
 
     Fir1 fir(nc);
     fir.setLearningRate(lrate);
     int ns (sr*ss);
-    std::list<double> wetList = std::list<double>();
-    std::list<double> dryList = std::list<double>();
+    std::vector<double> wetVector = std::vector<double>();
+    std::vector<double> dryVector = std::vector<double>();
+    std::vector<double> error = std::vector<double>();
     for (int i = 0; i < nc; i++)
     {
       wet.get();  
@@ -39,15 +43,59 @@ int main(int argc, char* argv[]){
     double sample(1);
     while (sample != 0){
         sample = wet.get();
-        wetList.push_back(sample);
+        wetVector.push_back(sample);
     };
     sample = 1;
     while (sample != 0){
         sample = dry.get();
-        dryList.push_back(sample);
+        dryVector.push_back(sample);
     };
-    //std::cout << wetList << std::endl;
-    //std::cout << dryList << std::endl;
+    if (wetVector.size() != dryVector.size()){
+        std::cerr << "interference and source samples are different lengths"<< std::endl;
+    }
+    double msi(0);
+    for (int i = 0; i < wetVector.size(); i++)
+    {
+        msi += pow(wetVector[i],2);
+    }
+    msi = msi / wetVector.size();
+    int actTrial(0);
+    if (trial < wetVector.size()){
+        actTrial = trial;
+    }else
+    {
+        actTrial = wetVector.size();
+    }
+    std::cout << "pre-training ..."<< std::endl;
+    for (int i = 0; i < actTrial; i++)
+    {
+        fir.lms_update(wetVector[i] - fir.filter(dryVector[i]));
+    }
+    std::cout << "Processing" <<std::endl;
+    for (int i = 0; i < dryVector.size(); i++)
+    {
+        error.push_back(wetVector[i] - fir.filter(dryVector[i]));
+        fir.lms_update(error[i]);
+    }
+    double powerGain (0);
+    for (int i = 0; i < error.size(); i++)
+    {
+        powerGain += pow(error[i],2);
+    }
+    powerGain = powerGain/error.size();
+    powerGain = powerGain/msi;
+    std::cout << "Power gain: " << powerGain << std::endl;
+    
+    
+    
+
+    
+    
+    
+    
+
+    //std::cout << wetVector << std::endl;
+    //std::cout << dryVector << std::endl;
     return 0;
 }
 
